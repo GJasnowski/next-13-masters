3dcc616bb3a30f5f2afdf4f5ea2e75a2de778aec
GJasnowski <gustaw.jasnowski@gmail.com>
2024-01-17 20:10:35 +0100

    generateStaticParams for products

--- src/app/(with-navigation)/products/[[...routes]]/page.tsx
+++ src/app/(with-navigation)/products/[[...routes]]/page.tsx
@@ -3,10 +3,17 @@ import { ProductsList } from "../../../../ui/organisms/ProductsList";
 import { Title } from "@/ui/atoms/Title";
 import { getProducts } from "@/api/products";
 import { stringToNumber } from "@/utils/general";
+import { productsPagesCount } from "@/utils/constants";
 
 export const metadata: Metadata = {
 	title: "All products",
 	description: "All products",
+};
+
+export const generateStaticParams = () => {
+	return [...Array(productsPagesCount).keys()].map((page) => {
+		routes: [`${page}`];
+	});
 };
 
 export default async function Products({
