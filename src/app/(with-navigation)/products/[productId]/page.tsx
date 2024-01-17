eabac0f1f78429574d457d64ab5a960d0987b1e7
GJasnowski <gustaw.jasnowski@gmail.com>
2024-01-17 17:27:27 +0100

    metadata

--- src/app/(with-navigation)/products/[productId]/page.tsx
+++ src/app/(with-navigation)/products/[productId]/page.tsx
@@ -1,5 +1,18 @@
+import { type Metadata } from "next";
 import { getProduct } from "@/api/products";
 import { ProductShowcase } from "@/ui/organisms/ProductShowcase";
+
+export const generateMetadata = async ({
+	params: { productId },
+}: {
+	params: { productId: string };
+}): Promise<Metadata> => {
+	const product = await getProduct(productId);
+	return {
+		title: product.title,
+		description: product.description,
+	};
+};
 
 export default async function Product({
 	params: { productId },
