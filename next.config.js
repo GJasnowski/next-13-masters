50cbff95dfa75660e2371381786468343d8c9061
GJasnowski <gustaw.jasnowski@gmail.com>
2024-01-18 18:42:15 +0100

    Use next Image component

--- next.config.js
+++ next.config.js
@@ -4,6 +4,15 @@ const nextConfig = {
 	experimental: {
 		typedRoutes: true,
 	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "media.graphassets.com",
+				pathname: "**",
+			},
+		],
+	},
 	async redirects() {
 		return [{ source: "/products", destination: "/products/1", permanent: false }];
 	},
